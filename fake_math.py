0b4e19609cad099201cde4d73d16f9a714d06eb2
Remgrad <remgrad622@gmail.com>
2024-12-03 22:24:32 +0300

    Модули и пакеты

---
+++ fake_math.py
@@ -0,0 +1,10 @@
+def fake_divide (first, second):
+    if second != 0:
+        return first / second
+    else:
+        return ('Ошибка')
+
+# result1 = divide(69, 3)
+# result2 = divide(3, 0)
+# print(result1)
+# print(result2)
---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+import fake_math as f_d
+import true_math as t_d
+
+
+result1 = f_d.fake_divide(69, 3)
+result2 = f_d.fake_divide(3, 0)
+result3 = t_d.true_divide(49, 7)
+result4 = t_d.true_divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
